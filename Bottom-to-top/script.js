f6888fe11d0b37af7698faebf205abb09f292d34
Alish Pawn <alishpau@gmail.com>
2024-12-17 10:00:01 +0545

    change style in bottom top

--- Bottom-to-top/script.js
+++ Bottom-to-top/script.js
@@ -1,14 +1,16 @@
-var btn = $('#bottom-top');
+jQuery(function($){
+    var btn = $('#bottom-top');
 
-$(window).scroll(function() {
-    if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
-    } else {
-    btn.removeClass('show');
-    }
-});
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 300) {
+        btn.addClass('show');
+        } else {
+        btn.removeClass('show');
+        }
+    });
 
-btn.on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({scrollTop:0}, '300');
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
 });
